a5ad076ab470c6f68257db5b1cc4db8df27f7329
Priyashree S <priya_sudarshanam@harvard.edu>
2014-02-14 14:24:02 -0500

    added another line to the function

--- samplecode/helloworld.js
+++ samplecode/helloworld.js
@@ -1,5 +1,6 @@
 function sayHello()
 {
-  document.getElementById("hello").innerHTML = '<b><font color="red">Hello World</font></b>';
+  document.getElementById("hello").innerHTML = '<b>Hello World</b>';
+  document.getElementById("changecolor").innerHTML = '<b><font color="red">Hello World</font></b>';
 }
 window.onload = sayHello;
