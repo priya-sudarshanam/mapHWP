67f83e55bdd611c0518119df1140973cf27ad732
wuqiank <wuqiank@gmail.com>
2014-02-25 16:44:01 -0500

    add a sample of marker

--- qi/map_example/source.js
+++ qi/map_example/source.js
@@ -1,11 +1,28 @@
-function initialize()
-{
-var mapProp = {
-  center:new google.maps.LatLng(42.373573,-71.122887),
-  zoom:13,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+function initialize(){
+
+	var mapProp = {
+		center:new google.maps.LatLng(42.373573,-71.122887),
+		zoom:13,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	
+	var addr1=new google.maps.LatLng(42.373573,-71.122887);
+	var marker1=new google.maps.Marker({position:addr1, 
+										animation:google.maps.Animation.DROP});
+	
+	var infowindow = new google.maps.InfoWindow({
+							content:"Hello World!"
+							});
+
+	google.maps.event.addListener(marker1, 'mouseover', function() {
+							infowindow.open(map,marker1);
+							});
+	google.maps.event.addListener(marker1, 'mouseout', function() {
+							infowindow.close(map,marker1);
+							});
+	
+	marker1.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
