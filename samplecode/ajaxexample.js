90981c23f5f560216fb838d29844a0795cd88b9b
Priyashree S <priya_sudarshanam@harvard.edu>
2014-02-28 20:57:24 -0500

    Delete ajaxexample.js

--- samplecode/ajaxexample.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    Drupal.behaviors.ajaxexample = {
-            attach:function(context, settings){
-                    $("#ajax-example-link").click(function()  {
-                      $("#ajax-display").text('thank you');
-      });;
-            }
-    };
-})(jQuery);
