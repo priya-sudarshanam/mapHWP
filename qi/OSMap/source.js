2e03e40819eb4cacd392973539ea1532e59c03af
wuqiank <wuqiank@gmail.com>
2014-03-05 11:27:14 -0500

    tagged map, list, add form

--- qi/OSMap/source.js
+++ qi/OSMap/source.js
@@ -1,19 +1,42 @@
+
 function initialize(){
 
-	var lat = Drupal.settings['OSMap'][0]['lat'];
-	var lng = Drupal.settings['OSMap'][0]['lng'];
-		
+	var len = Drupal.settings['OSMap'].length;
+	var lat;
+	var lng;
+	var addr;
+	var markers = new Array();
+	var infowindows = new Array();
+	
 	var mapProp = {
-		center:new google.maps.LatLng(lat,lng),
-		zoom:13,
-		mapTypeId:google.maps.MapTypeId.ROADMAP
+		center:new google.maps.LatLng(42.394450, -71.093227),
+		zoom:12,
+		mapTypeId:google.maps.MapTypeId.ROADMAP,
+		
+		// disable the default map UI
+		disableDefaultUI: true,
+		
+		// prevents map from being dragged
+        draggable: false,
+		
+		// prevents map from being double click zoomed
+		disableDoubleClickZoom: true,
+		
+		zoomControl: false,
 	};
-	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 	
-	var addr1=new google.maps.LatLng(lat,lng);
-	var marker1=new google.maps.Marker({position:addr1, 
+	map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	
+	for (var i=0; i<len; i++) {
+		lat = Drupal.settings['OSMap'][i]['lat'];
+		lng = Drupal.settings['OSMap'][i]['lng'];
+		addr = new google.maps.LatLng(lat,lng);
+		markers[i] = new google.maps.Marker({position:addr, 
 										animation:google.maps.Animation.DROP});
+		markers[i].setMap(map);
+	}
 	
+	/*
 	var infowindow = new google.maps.InfoWindow({
 							content:"Hello World!"
 							});
@@ -24,8 +47,9 @@ function initialize(){
 	google.maps.event.addListener(marker1, 'mouseout', function() {
 							infowindow.close(map,marker1);
 							});
-	
-	marker1.setMap(map);
+	*/
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
